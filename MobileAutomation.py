e1958165e4d922f1021666dfd4558b420875a55e
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-02-16 22:21:41 +0530

    Add files via upload

---
+++ MobileAutomation.py
@@ -0,0 +1,21 @@
+import datetime
+import time
+import os
+import termuxScheduler
+
+
+def main():
+    while True:
+        hour = datetime.datetime.now().hour - 12 if datetime.datetime.now().hour > 13 else datetime.datetime.now().hour
+        meridian = " PM" if datetime.datetime.now().hour >= 12 else " AM"
+        current_time = str(hour) + ':' + str(datetime.datetime.now().minute) + meridian
+        if current_time in termuxScheduler.dictionary:
+            print(current_time)
+            os.system(termuxScheduler.dictionary[current_time])
+            time.sleep(45)
+        else:
+            time.sleep(35)
+
+
+if __name__ == '__main__':
+    main()
