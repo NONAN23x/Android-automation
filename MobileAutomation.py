d25ee8f83e36c99d0f4629b6a98e3f4d3bd06c65
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-02-19 11:20:29 +0530

    Update MobileAutomation.py

--- MobileAutomation.py
+++ MobileAutomation.py
@@ -5,7 +5,7 @@ import termuxScheduler  # This is a local module
 
 
 def main():  # Actual piece of code
-    while True:  # True condition for running the program forever
+       while True:  # True condition for running the program forever
         hour = datetime.datetime.now().hour - 12 if datetime.datetime.now().hour > 13 else datetime.datetime.now().hour
         # Here 'hour' variable is configured to use 12 hour timezone, if you prefer 24 hours...
         # then tune this logic yourself! ;)
@@ -14,13 +14,18 @@ def main():  # Actual piece of code
         # Actual variable I'll be using to refer to call actual time
         print(current_time)  # Yes, I use print statements for debugging ;)
         ###
-        if current_time in termuxScheduler.dictionary:
-            # And here comes the actual part where python is given decision making privilege            
-            os.system(termuxScheduler.dictionary[current_time])
-            # This is the piece of code that's making system commands
-            time.sleep(45)  # sleep method for delaying the process
-        else:
-            time.sleep(35)
+        try:    # Try and except for dealing with Keyboard Interrupt, this results in clean code
+            if current_time in termuxScheduler.dictionary:
+                # And here comes the actual part where python is given decision making privilege
+                os.system(termuxScheduler.dictionary[current_time])
+                # This is the piece of code that's making system commands
+                time.sleep(40)  # sleep method for delaying the process
+            else:
+                time.sleep(30)
+        except KeyboardInterrupt:
+            print("^Exit")
+            print("Exiting...")
+            break
         ###
 
 if __name__ == '__main__':  # Professional way to run a code, right?
