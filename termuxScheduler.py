331e11c0b8ac71c456ba2fe1ab0e93972955417d
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-02-20 16:33:11 +0530

    Update termuxScheduler.py

--- termuxScheduler.py
+++ termuxScheduler.py
@@ -1,14 +1,22 @@
-
 def notification(string):
-    return "termux-notification -c Notification -t " + string
+    return f"termux-notification -c Notification -t {string}"
+
+
 def toast(string):
-    return "termux-toast -b black -c white " + string
+    return f"termux-toast -b black -c white {string}"
+
+
 def torch(string):
-    return "termux-torch" + string               # These functions are simplified methods for using system calls
+    return f"termux-torch {string}"  # These functions are simplified methods for using system calls
+
+
 def speak(string):
-    return "termux-tts-speak" + string
+    return f"termux-tts-speak {string}"
+
+
 def volume(string):
-    return "termux-volume" + string
+    return f"termux-volume {string}"
+
 
 dictionary = {
 
@@ -16,18 +24,18 @@ dictionary = {
     "6:0 AM": toast("Hello"),
     "6:30 AM": toast("ITs 630"),
     "7:0 AM": notification("Its 7 o clock"),
-    "7:30 AM": " ",
-    "8:0 AM": " ",
-    "8:30 AM": " ",
-    "9:0 AM": " ",
-    "9:30 AM": " ",
-    "10:0 AM": " ",
-    "10:30 AM": " ",
-    "11:0 AM": " ",
-    "11:30 AM": " ",
-    "12:0 PM": " ",
-    "12:30 PM": " ",
-    "1:0 PM": " ",
+    "7:30 AM": notification("wake up"),
+    "8:0 AM": notification("Ready up please"),
+    "8:30 AM": notification("wake up up"),
+    "9:0 AM": notification("It's 9"),
+    "9:30 AM": notification("Its 930 "),
+    "10:0 AM": notification("It's 10"),
+    "10:30 AM": notification("its 1030"),
+    "11:0 AM": notification("Its 11"),
+    "11:30 AM": notification("Its 1130"),
+    "12:0 PM": notification("it's 12 AM"),
+    "12:30 PM": notification("12:30"),
+    "1:0 PM": notification("1:00"),
     "1:30 PM": " ",
     "2:0 PM": " ",
     "2:30 PM": " ",
@@ -36,7 +44,7 @@ dictionary = {
     "4:0 PM": " ",
     "4:30 PM": " ",
     "5:0 PM": " ",
-    "5:30 PM": notification(" LOL 530"),
+    "5:30 PM": notification("LOL 530"),
     "6:0 PM": notification("LOl 600"),
     "6:30 PM": notification("LOL 630"),
     "7:0 PM": notification("LOL 7"),
