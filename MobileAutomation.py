16ecb6d1f75dae03ff5b448685d642b33fb468bd
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-02-25 10:21:25 +0530

    Update MobileAutomation.py

--- MobileAutomation.py
+++ MobileAutomation.py
@@ -5,19 +5,22 @@ import termuxScheduler  # This is a local module
 
 
 def main():  # Actual piece of code
-       while True:  # True condition for running the program forever
-        hour = datetime.datetime.now().hour - 12 if datetime.datetime.now().hour > 13 else datetime.datetime.now().hour
+    while True:  # True condition for running the program forever
+        hour = datetime.datetime.now().hour - 12 if datetime.datetime.now().hour >= 13 else datetime.datetime.now().hour
         # Here 'hour' variable is configured to use 12 hour timezone, if you prefer 24 hours...
         # then tune this logic yourself! ;)
         meridian = " PM" if datetime.datetime.now().hour >= 12 else " AM"  # This is self explanatory
         current_time = str(hour) + ':' + str(datetime.datetime.now().minute) + meridian
         # Actual variable I'll be using to refer to call actual time
-        print(current_time)  # Yes, I use print statements for debugging ;)
+        if current_time not in termuxScheduler.dictionary:
+            print(current_time, "    (no task was assigned)")  # Yes, I use print statements for debugging ;)
+        else:
+            print(current_time, termuxScheduler.dictionary[current_time])
         ###
-        try:    # Try and except for dealing with Keyboard Interrupt, this results in clean code
+        try:  # Try and except for dealing with Keyboard Interrupt, for clean code
             if current_time in termuxScheduler.dictionary:
                 # And here comes the actual part where python is given decision making privilege
-                os.system(termuxScheduler.dictionary[current_time])
+                print(termuxScheduler.dictionary[current_time])
                 # This is the piece of code that's making system commands
                 time.sleep(40)  # sleep method for delaying the process
             else:
@@ -28,8 +31,6 @@ def main():  # Actual piece of code
             break
         ###
 
-              
+
 if __name__ == '__main__':  # Professional way to run a code, right?
     main()
-
-# Any feedback is appreciated
