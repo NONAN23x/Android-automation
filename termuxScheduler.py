cc9c714b33a1eac3bbd1e9739ab53bbd2ddc03f8
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-01 12:37:51 +0530

    Update termuxScheduler.py

--- termuxScheduler.py
+++ termuxScheduler.py
@@ -1,59 +1,59 @@
 def notification(string, sound="notification1"):
-    return f"termux-media-player play /Media/{sound} && termux-vibrate -f && termux-notification -t Notification -c '{string}'"
+    return f"termux-media-player play /Media/{sound} && termux-vibrate -f && termux-notification -t Notification -c \"{string}\""
 
 
 def torch(string):
-    return "termux-torch '" + string + "'"  # These functions are simplified methods for using system calls
+    return f"termux-torch {string}"  # These functions are simplified methods for using system calls
 
 
 def speak(string):
-    return f"termux-tts-speak '{string}'"
+    return f"termux-tts-speak \"{string}\""
 
 
 def volume(string):
-    return f"termux-volume '{string}'"
+    return f"termux-volume \"{string}\""
 
 
 dictionary = {
 
-    "5:30 AM": notification("LOL"),
-    "5:45 AM": notification("s"),
-    "6:0 AM": notification("s"),
-    "6:30 AM": notification("ITs 630"),
-    "7:0 AM": notification("Its 7 o clock"),
-    "7:30 AM": notification("wake up"),
-    "8:0 AM": notification("Ready up please"),
-    "8:30 AM": notification("wake up up"),
-    "9:0 AM": notification("It's 9"),
-    "9:30 AM": notification("Its 930 "),
-    "10:0 AM": notification("It's 10"),
-    "10:30 AM": notification("its 1030"),
-    "11:0 AM": notification("Its 11"),
+    "5:30 AM": notification("Its's 5:30, wake up"),
+    "5:45 AM": notification("Brush your teeth now"),
+    "6:0 AM": notification("Workout"),
+    "6:30 AM": notification("Now practise"),
+    "7:0 AM": notification("It's 7AM"),
+    "7:30 AM": notification("Its 7:30 AM"),
+    "8:0 AM": notification("Get ready for college"),
+    "8:30 AM": notification("You're supposed to be in college now"),
+    "9:0 AM": notification("NOthing Special"),
+    "9:30 AM": notification("Nothing Special"),
+    "10:0 AM": notification("Nothing Special"),
+    "10:30 AM": notification("Nothing special"),
+    "11:0 AM": notification("Nothing SPecial"),
     "11:30 AM": notification("Its 1130"),
     "12:0 PM": notification("it's 12 AM"),
     "12:30 PM": notification("12:30"),
-    "1:0 PM": notification("1:00"),
-    "1:30 PM": notification("LOL its 1:30"),
-    "2:0 PM": notification('lol'),
-    "2:30 PM": notification('lol'),
-    "3:0 PM": notification("lol"),
-    "3:30 PM": notification("A"),
-    "4:0 PM": notification("g"),
-    "4:30 PM": notification("a"),
-    "5:0 PM": notification("g"),
-    "5:30 PM": notification("LOL 530"),
-    "6:0 PM": notification("LOl 600"),
-    "6:30 PM": notification("LOL 630"),
-    "7:0 PM": notification("LOL 7"),
-    "7:30 PM": notification("LOL 730"),
-    "8:0 PM": notification("LOL 8"),
-    "8:30 PM": notification("LOL 830"),
-    "9:0 PM": notification("LOL 9"),
-    "9:30 PM": notification("LOL 930"),
-    "10:0 PM": notification("LOL 10"),
-    "10:30 PM": notification("LOL 1030"),
-    "11:0 PM": notification("LOL 11"),
-    "11:30 PM": notification("GEt back home you fool"),
+    "1:0 PM": notification("Time for Prayer"),
+    "1:30 PM": notification("Duolingo Time"),
+    "2:0 PM": notification('Bring some groceries to home'),
+    "2:30 PM": notification('Duolingo again'),
+    "3:0 PM": notification("Time for some errands"),
+    "3:30 PM": notification("Complete your daily chores"),
+    "4:0 PM": notification("Time for Prayer"),
+    "4:30 PM": notification("Get ready for tuition"),
+    "5:0 PM": notification("Study well"),
+    "5:30 PM": notification("Focus on studying"),
+    "6:0 PM": notification("More 60 minutes left"),
+    "6:30 PM": notification("30 minutes left"),
+    "7:0 PM": notification("Time to leave, reach home now"),
+    "7:30 PM": notification("Errands to run"),
+    "8:0 PM": notification("Pharmacy time"),
+    "8:30 PM": notification("Work"),
+    "9:0 PM": notification("Maybe consider reaching home"),
+    "9:30 PM": notification("Time for some Duolingo"),
+    "10:0 PM": notification("Time for some gaming maybe?"),
+    "10:30 PM": notification("Think of wrapping up things"),
+    "11:0 PM": notification("GEt to sleep"),
+    "11:30 PM": notification("Sweet dreams"),
 
 }
 # You can configure this dictionary to your preference
