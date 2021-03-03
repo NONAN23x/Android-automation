cc7df44db42f099e78a951deffa3f06661d03218
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:40:45 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-FULL="PLUGGED_AC"
-VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
+while [ True ]
+do
+    FULL="PLUGGED_AC"
+    VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
 
-if [[ "$VAR3" -ne "$FULL" ]]; then
-    echo "Not plugged"
-elif [[ "$VAR3" -eq "$FULL" ]]; then
-    echo "Cable connected"
-fi
+    if [[ "$VAR3" -ne "$FULL" ]]; then
+        echo "Not plugged"
+    elif [[ "$VAR3" -eq "$FULL" ]]; then
+        echo "Cable connected"
+    fi
+DONE
