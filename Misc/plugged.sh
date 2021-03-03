1d2d7b3b470c38d5ec04f9623c14eae9ccb03896
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 15:03:06 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -6,10 +6,7 @@ do
     VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
     ALLOW=TRUE
     if [[ "$VAR3" = "$FULL" ]]; then
-        if [[ $ALLOW ]]
-            termux-tts-speak "Charger connected"
-            $ALLOW = FALSE
-        fi
+        termux-tts-speak "Charger Connected"
     else
         ALLOW=TRUE
         sleep 1
