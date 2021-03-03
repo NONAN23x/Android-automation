72a3e0b3fd5de33ae54c4ada3f4f41b262cd1e16
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 09:51:26 +0530

    Update MobileAutomation.py

--- MobileAutomation.py
+++ MobileAutomation.py
@@ -13,6 +13,10 @@ light_green = colorama.Fore.LIGHTGREEN_EX
 light_cyan = colorama.Fore.LIGHTCYAN_EX
 finish = colorama.Style.RESET_ALL
 
+def fetch_battery():
+    battery = os.system("python -c 'import os;os.system('termux-battery-status | grep percentage | cut -d ':' -f 2')'")
+    print(battery)
+
 
 def main():  # Actual piece of code
     while True:  # True condition for running the program forever
@@ -22,9 +26,9 @@ def main():  # Actual piece of code
         meridian = " PM" if datetime.datetime.now().hour >= 12 else " AM"  # This is self explanatory
         current_time = str(hour) + ':' + str(datetime.datetime.now().minute) + meridian
         # Actual variable I'll be using to refer to call actual time
-        battery = str(os.system("termux-battery-status | grep percentage | cut -d ':' -f 2"))
         
-        print(battery)
+        fetch_battery()
+        
         if current_time not in Schedule.dictionary:
             print(light_cyan, current_time, "    (No task)", finish)  # Yes, I use print statements for debugging ;)
         else:
