dc202198dec09bfb8ff9c054eb0425a2c08bfd56
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:42:43 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -6,8 +6,10 @@ do
     VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
 
     if [[ "$VAR3" -ne "$FULL" ]]; then
-        echo "Not plugged"
+        echo $VAR3
+        echo $FULL
     elif [[ "$VAR3" -eq "$FULL" ]]; then
-        echo "Cable connected"
+        echo $VAR3
+        echo $FULL
     fi
 done
