ac7047b7b5b83a99317d381db377ca95536d602b
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:27:22 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -4,10 +4,15 @@
 while [ True ]
 do
     YES="PLUGGED_AC"
+    NOT="UNPLUGGED"
     VAR1="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
+    
+    
     if [[ "$VAR1" -eq "$YES" ]]; then
         echo "CHARGER CONNECTED"
-    else
-        echo "CHARGER NOT CONNECTED" 
+    elif [[ "$VAR1" -eq "$NOT" ]]; then
+        echo "NOT CONNECTED"
     fi
+    
+    
 done
