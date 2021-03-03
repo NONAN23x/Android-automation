ca5e825b6f4a149d1485c93d942a75255d787158
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:31:09 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -3,7 +3,7 @@
 
 FIFTY="UNPLUGGED"
 FULL="PLUGGED_AC"
-VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1)"
+VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
 
 if [[ "$VAR3" -eq "$FIFTY" ]]; then
     termux-tts-speak "Please connect the charger"
