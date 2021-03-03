9ebf4952bf5bc921b4a68c2da7ffb99604113ed6
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:36:55 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
-FIFTY="UNPLUGGED"
 FULL="PLUGGED_AC"
 VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
 
-if [[ "$VAR3" -eq "$FIFTY" ]]; then
-    termux-tts-speak "Please connect the charger"
+if [[ "$VAR3" -nq "$FULL" ]]; then
+    echo "Not plugged"
 elif [[ "$VAR3" -eq "$FULL" ]]; then
-    termux-tts-speak "Charger connected"
+    echo "Cable connected"
 fi
