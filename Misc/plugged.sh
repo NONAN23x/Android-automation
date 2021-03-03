b574218b686b560e8de71857a57145c1a58bb1a9
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 15:09:11 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -6,10 +6,12 @@ do
     VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
     ALLOW=TRUE
     if [[ "$VAR3" = "$FULL" ]]; then
+        echo "Charger Connected"
         termux-tts-speak "Charger Connected"
+        sleep 2
     else
-        ALLOW=TRUE
-        sleep 1
+        echo "Charget not Connected"
+        sleep 2
        
     fi
 done
