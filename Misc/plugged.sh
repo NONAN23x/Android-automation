60ddf85528c2ad2eefda5261d49161bd465fd063
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:15:56 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -4,6 +4,6 @@ while [ True ]
 do
     VAR1="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
     if [[ "$VAR1" -eq "$YES" ]]; then
-    termux-tts-speak "My battery is low, please charge immediately"
+    termux-tts-speak "CHARGER CONNECTED"
     fi 
 done
