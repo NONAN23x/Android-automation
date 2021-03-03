be5acc0ae71cc150465dfa1b6cfd77e9ec556b94
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:30:30 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-TWENTYFIVE="25"
-FIFTY="50"
-FULL="99"
+
+FIFTY="UNPLUGGED"
+FULL="PLUGGED_AC"
 VAR3="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1)"
 
-if [[ "$VAR3" -eq "$TWENTYFIVE" ]]; then
-    termux-tts-speak "My battery is low, please charge immediately"
-elif [[ "$VAR3" -eq "$FIFTY" ]]; then
-    termux-tts-speak "My battery is 50%"
+if [[ "$VAR3" -eq "$FIFTY" ]]; then
+    termux-tts-speak "Please connect the charger"
 elif [[ "$VAR3" -eq "$FULL" ]]; then
-    termux-tts-speak "Device fuly charged, you may unplug now "
+    termux-tts-speak "Charger connected"
 fi
