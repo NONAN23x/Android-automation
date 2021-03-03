43f96a0416db4850a73a6c45c6e5aabb4e9eaef5
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-03-03 14:15:12 +0530

    Update plugged.sh

--- Misc/plugged.sh
+++ Misc/plugged.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-NOT="UNPLUGGED"
 YES="PLUGGED"
 while [ True ]
 do
     VAR1="$(termux-battery-status | grep plugged | cut -d ':' -f 2 | cut -d ',' -f 1 | cut -d '"' -f 2)"
-    if [[ "$VAR1" -eq "$NOT" ]]; then
+    if [[ "$VAR1" -eq "$YES" ]]; then
     termux-tts-speak "My battery is low, please charge immediately"
-    elif [[ "$VAR1" -eq "$YES" ]]; then
-    termux-tts-speak "My battery is 50%" 
     fi 
 done
